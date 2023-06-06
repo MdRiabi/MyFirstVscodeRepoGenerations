ff9d0668e63b21505de3d20a3fa9fc2983aedb79
MdRiabi <mhdriabi2013@gmail.com>
2023-06-06 21:29:13 +0200

    basics math operations implementation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,56 @@
 //alert('this work');
+const  defaultResult =0;
+let currentResult = defaultResult;
+
+// get input value from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+// generates and writes calculation log
+function createAndWriteOutput(opertor,  resultBeforeCalc, calcNumber){
+
+    const calcDescription = `${resultBeforeCalc}${opertor}${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add(){
+    const entredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + entredNumber;
+    createAndWriteOutput('+' , initialResult , entredNumber);
+}
+
+function subtract(){
+    const entredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - entredNumber;
+    createAndWriteOutput('-' , initialResult , entredNumber);
+}
+
+function multiply(){
+    const entredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * entredNumber;
+    createAndWriteOutput('*' , initialResult , entredNumber);
+}
+
+function divide(){
+    const entredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / entredNumber;
+    createAndWriteOutput('/' , initialResult , entredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
-let currentResult = 10;
+
+
+
 
 //currentResult = currentResult + 10;
-outputResult(currentResult , '');
+//let abc = '(' +currentResult+ '+1+12)+ 1';
+//outputResult(currentResult , abc);
