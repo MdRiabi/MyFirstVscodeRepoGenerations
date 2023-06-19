dfc31c21b2928995556c5e622effdc1d85302545
MdRiabi <mhdriabi2013@gmail.com>
2023-06-20 01:51:06 +0200

    explainning the callback function

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -148,3 +148,22 @@ output   {
     lastname: "Patidar"
 }*/
 
+const sumUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validateNumber(num);
+  }
+  resultHandler(sum);
+};
+
+
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+
+};
+
+sumUp(showResult, 1, 5, 'fdsa', -3, 6, 10);
