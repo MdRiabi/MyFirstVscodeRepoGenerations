a8b5a987505346da0fbe24c66c43b5fd2830bb98
MdRiabi <mhdriabi2013@gmail.com>
2023-06-20 02:40:38 +0200

    bind() method

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -165,5 +165,69 @@ const showResult = (result) => {
   alert('The result after adding all numbers is: ' + result);
 
 };
-
 sumUp(showResult, 1, 5, 'fdsa', -3, 6, 10);
+
+
+
+
+
+
+
+const combine = (resultHandler, operation, ...numbers) => {
+  const validateNumber = number => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+  }
+  resultHandler(sum);
+};
+
+
+const showResults = (messageText, result) => {
+  alert(messageText + ' ' + result);
+};
+
+combine(
+  showResults.bind(this, 'The result after adding all numbers is:'),
+  'ADD',
+  1,
+  5,
+  'fdsa',
+  -3,
+  6,
+  10
+);
+
+combine(
+  showResult.bind(this, 'The result after subtracting all numbers is:'),
+  'SUBTRACT',
+  1,
+  10,
+  15,
+  20
+);
+
+
+//With the bind() method, an object can borrow a method from another object.
+
+const person = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const member = {
+  firstName:"Hege",
+  lastName: "Nilsen",
+}
+
+let fullName = person.fullName.bind(member);   // output is :  Hege Nilsen
